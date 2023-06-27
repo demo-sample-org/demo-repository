03d7a5ae410e79d9bc6b58357e42b9938e116178
pshara6644 <137052574+pshara6644@users.noreply.github.com>
2023-06-27 16:40:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,39 @@
          }
        }
      }
+provider "azurerm" {
+  features {}
+}
 
-     # An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+resource "azurerm_resource_group" "example" {
+  name     = "demo-resource-group"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "demo-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_web_app" "example" {
+  name                = "demo-web-app"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  server_farm_id      = azurerm_app_service_plan.example.id
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "JAVA|11-java11"
+  }
+}
+
+output "web_app_url" {
+  value = azurerm_web_app.example.default_site_hostname
+}
